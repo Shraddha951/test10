33e2f86fa9d10cad0b28ee3b7f593e6456c6405f
Shraddha951 <amboreshraddha3@gmail.com>
2025-10-19 02:59:42 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -8,7 +8,7 @@ public class Test4Application {
 
 	public static void main(String[] args) {
                 int x = 100;
-		SpringApplication.run(Test4Application.class, args);
+		        SpringApplication.run(Test4Application.class, args);
 	}
 
 }
